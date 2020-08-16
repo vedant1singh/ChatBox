6583d30a5b0aa7af039bd0f84e56f5391e18a41d
vedant singh <deepaksinghvedu@gmail.com>
2020-08-16 22:53:03 +0530

    ChatBox

---
+++ Box/Email/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+def index(request):
+    if request.method == "POST":
+        message = request.POST['message']
+        send_mail("Contact Form",
+                  message,
+                  settings.EMAIL_HOST_USER,
+                  ["deepaksinghvedu@gmail.com"],
+                  fail_silently=False)
+    return render(request, 'index.html')
